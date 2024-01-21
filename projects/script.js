a6cc0f1042595b8bb93490165801f6a6fc96d705
Saksham Jain <137368917+Sakshamjain98@users.noreply.github.com>
2024-01-21 23:01:15 +0530

    Add files via upload

---
+++ projects/script.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+
+    $('#menu').click(function() {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toogle');
+
+        if (window.scrollY > 60) {
+            document.querySelector('#scroll-top').classList.add('active');
+        } else {
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+    })
+});
+
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Projects | Portfolio Saksham Jain";
+            $("#favicon").attr("href", "/assets/images/favicon.png");
+        }
+        else {
+            document.title = "Come Back To Portfolio";
+            $("#favicon").attr("href", "/assets/images/favhand.png");
+        }
+    });
+
+/* Fetch Proect Start */
+
+function getProjects(){
+    return fetch("projects.json")
+    .then(response=> response.json())
+    .then(data=>{
+        return data;
+    });
+}
+
+function showProjects(projects){
+    let projectscontainer = document.querySelector(".work .box-container");
+    let projectsHTML= "";
+    projects.forEach(project => {
+        projectsHTML+=
+        `
+        <div class="grid-item ${project.category}">
+        <div class="box tilt" style="width: 380px; margin: 1rem">
+      <img draggable="false" src="/assets/images/projects/${project.image}.jpg" alt="project" />
+      <div class="content">
+        <div class="tag">
+        <h3>${project.name}</h3>
+        </div>
+        <div class="desc">
+          <p>${project.desc}</p>
+          <div class="btns">
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>`
+    });
+
+    projectscontainer.innerHTML=projectsHTML;
+
+      // isotope filter products
+      var $grid = $('.box-container').isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
+        masonry: {
+            columnWidth: 200
+        }
+    });
+
+    // filter items on button click
+    $('.button-group').on('click', 'button', function () {
+        $('.button-group').find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+}
+
+getProjects().then(data => {
+    showProjects(data);
+})
+
+    // disable developer mode
+document.onkeydown = function (e) {
+    if (e.keycode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keycode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keycode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keycode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keycode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
